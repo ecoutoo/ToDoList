3b233c28416383147087b9ef9b48cae026d9a29e
ecoutoo <viccontecouto@gmail.com>
2021-03-16 16:08:07 +0000

    TDL-27 #Created mapper class

--- src/main/java/com/qa/tdl/mappers/ToDoListMapper.java
+++ src/main/java/com/qa/tdl/mappers/ToDoListMapper.java
@@ -1,5 +1,19 @@
 package com.qa.tdl.mappers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.qa.tdl.data.model.ToDoList;
+
+@Component
 public class ToDoListMapper {
+	
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	public ToDoListMapper(ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+	}
 
 }

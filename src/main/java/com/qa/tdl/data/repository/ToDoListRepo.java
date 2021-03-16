9daa946747fb5f02e14f122320a4235ae48f023f
ecoutoo <viccontecouto@gmail.com>
2021-03-16 16:01:03 +0000

    TDL-27 #created ToDoList repo interface

--- src/main/java/com/qa/tdl/data/repository/ToDoListRepo.java
+++ src/main/java/com/qa/tdl/data/repository/ToDoListRepo.java
@@ -1,5 +1,11 @@
 package com.qa.tdl.data.repository;
 
-public class ToDoListRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.tdl.data.model.ToDoList;
+
+@Repository
+public interface ToDoListRepo extends JpaRepository<ToDoList, Integer>{
 
 }

b79d83e254bd5c89d02082002a18496f83131a26
ecoutoo <viccontecouto@gmail.com>
2021-03-16 16:33:15 +0000

    TDL-27 #Created todolist service

--- src/main/java/com/qa/tdl/controller/ToDoListController.java
+++ src/main/java/com/qa/tdl/controller/ToDoListController.java
@@ -1,5 +1,117 @@
 package com.qa.tdl.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.duckdemo.Services.VibeService;
+import com.example.duckdemo.data.model.Vibe;
+import com.example.duckdemo.dto.VibeDTO;
+
+@RestController
+@RequestMapping(path = "/todolist") // This controller has a base path of /todolist (localhost:8080/todolist)
 public class ToDoListController {
+	
+	//localhost:8080/
+	//@RequestMapping(method = RequestMethod.GET)
+	
+	//@Autowired //field injection
+	private VibeService vibeService;
+	
+	@Autowired // constructor injection (injected from the application context)
+	public VibeController(VibeService vibeService) {
+		this.vibeService = vibeService;
+	}
+	
+	// localhost:8080/vibe
+	@GetMapping
+	public ResponseEntity<List<VibeDTO>> getAllVibe() {
+		
+		// Response has headers, a body and a status code
+		HttpHeaders httpHeaders = new HttpHeaders(); // Creating some headers
+		httpHeaders.add("Location", "1442"); // Adding a header
+		
+		// Requesting our vibeDTO data from the vibeService
+		List<VibeDTO> data = vibeService.checkAllVibes();
+		
+		// returning a response of type ResponseEntity(Body, Headers, HttpStatus)
+		return new ResponseEntity<List<VibeDTO>>(data, httpHeaders, HttpStatus.OK);
+	}
+	
+	// localhost:8080/vibe/3
+	@GetMapping("/{id}") // {id} is a path variable
+	public ResponseEntity<VibeDTO> getVibeById(@PathVariable("id") int id) {
+		// The path variable is captured by the @PathVariable annotation
+		// - WE MUST SUPPLY A MATCHING NAME WITHIN THE PARENTHESIS
+		// - WE MUST SUPPLY AN APPROPRIATE DATA TYPE FOR THE VAR TO CONVERT TO
+		
+		// Get our vibe data using the service
+		VibeDTO vibe = vibeService.readById(id);
+		
+		// Return the vibe data in a response
+		return new ResponseEntity<VibeDTO>(vibe, HttpStatus.OK);
+	}
+	
+	// Spring Boots version of @PathParam is @RequestParam(defaultValue = "")
+	// localhost:8080/vibe/alt?id=1
+	@GetMapping("/alt")
+	public ResponseEntity<VibeDTO> getVibeByIdAlt(@RequestParam("id") int id) {
+		// @RequestParam grabs a query parameter from our path
+		// - In this case, it is called `id` and MUST BE SUPPLIED
+		// - We can make it optional like so: @RequestParam(name = "id", required = false)
+		//   - Or @RequestParam(name = "id", defaultValue = "")
+		
+		
+		VibeDTO vibe = vibeService.readById(id);
+		
+		return new ResponseEntity<VibeDTO>(vibe, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<VibeDTO> createVibe(@Valid @RequestBody Vibe vibe) {
+		// A vibe is retrieved from the incoming request body (the conversion from json to vibe is automatic)
+		// - `@RequestBody vibe vibe` makes this happen
+		// - @Valid is used to employ our models validation on the incoming request
+		
+		VibeDTO newvibe = vibeService.createVibe(vibe);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", String.valueOf(newvibe.getId()));
+	
+		return new ResponseEntity<VibeDTO>(newvibe, headers, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<VibeDTO> updateVibe(@PathVariable("id") int id,
+										   @RequestBody Vibe vibe) {
+		VibeDTO updatedvibe = vibeService.updateVibe(id, vibe);
+		
+		return new ResponseEntity<VibeDTO>(updatedvibe, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteVibe(@PathVariable("id") int id) {		
+		return new ResponseEntity<Boolean>(vibeService.deleteVibe(id), HttpStatus.OK);
+	}
+	
+	// @GetMapping (retrieving something)
+	// @PostMapping (creating something)
+	// @PutMapping (generalised update)
+	// @PatchMapping (specific update)
+	// @DeleteMapping (deleting something)
 
 }
--- src/main/java/com/qa/tdl/services/ToDoListService.java
+++ src/main/java/com/qa/tdl/services/ToDoListService.java
@@ -1,5 +1,73 @@
 package com.qa.tdl.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.tdl.data.model.ToDoList;
+import com.qa.tdl.data.repository.ToDoListRepo;
+import com.qa.tdl.mappers.ToDoListMapper;
+
+@Service
 public class ToDoListService {
+	
+	 private ToDoListRepo toDoListRepository;	 
+	 private ToDoListMapper toDoListMapper;
+	 
+	 @Autowired
+	 public ToDoListService(ToDoListRepo toDoListRepository, ToDoListMapper toDoListMapper) {
+		 this.toDoListMapper = toDoListMapper;
+		 this.toDoListRepository = toDoListRepository;
+	 }
+	 public List<ToDoList> checkAllToDoLists(){
+		 List<ToDoList> toDoList= toDoListRepository.findAll();
+		 //List<ToDoList> toDoListDTOs= new ArrayList<ToDoList>();
+		 
+		 //toDoLists.forEach(toDoList -> toDoListDTOs.add(toDoListMapper.mapToDTO(toDoList)));
+		 return toDoList;
+	 }
+	 
+	 public ToDoList readById(Integer id) {
+		 Optional<ToDoList> toDoList = toDoListRepository.findById(id);
+		 if (toDoList.isPresent()) {
+			 return toDoList.get();
+		 }else {
+			 throw new EntityNotFoundException();
+		 }
+	 }
+	 
+	 public ToDoList createToDoList(ToDoList toDoList) {
+		 return toDoListRepository.save(toDoList);
+	 }
+	 
+	 public boolean deleteToDoList(Integer id) {
+		 if(!toDoListRepository.existsById(id)) {
+			 throw new EntityNotFoundException();
+		 }
+		 toDoListRepository.deleteById(id);
+		 
+		 boolean exists = toDoListRepository.existsById(id);
+		 
+		 return !exists;
+	 }
+	 
+	 public ToDoList updateToDoList(Integer id, ToDoList toDoList) {
+		 Optional<ToDoList> otoDoList = toDoListRepository.findById(id);
+		 ToDoList toDoList2;
+		 if (otoDoList.isPresent()) {
+			 toDoList2 = otoDoList.get();
+		 }else {
+			 throw new EntityNotFoundException();
+		 }
+		 toDoList2.setTitle(toDoList.getTitle());
+		 toDoList2.setCompleted(toDoList.getCompleted());
+		 toDoListRepository.save(toDoList2);
+		 return toDoList2;
+	 }
 
 }

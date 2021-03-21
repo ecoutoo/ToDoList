3c115c77854101b0e7f34ae2f12f60aeaa97532c
ecoutoo <viccontecouto@gmail.com>
2021-03-21 21:37:39 +0000

    TDL-29 #frontend read list

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
-function ReadAll() {
+let toDoListId = document.querySelector("#listNumForm");
+
+function readAll() {
+    clearInfo();
     fetch("http://localhost:8080/todolist", {
     method: 'get',    
     }) // 1  
@@ -10,8 +13,72 @@ function ReadAll() {
             return;
         }
         response.json() // 3
-        .then(data => console.info(data)) // 4
-    }).catch((err)=> console.error(`${err}`)); // 5
+        .then(data => {
+            console.info(data)
+            for (var i = 0; i < data.length; i++) {
+                displayList(data[i])
+            }
+            querySuccess();
+        }) // 4
+    }).catch((err)=> {
+        console.error(`${err}`)
+        queryFailure();
+    }); // 5
+}
+
+function readOne() {
+    clearInfo();
+    fetch("http://localhost:8080/todolist/" + toDoListId.value, {
+    method: 'get',    
+    }) // 1  
+    .then((response) => {
+        if (response.status !== 200) {  //  2
+            console.error(`status: ${reponse.status}`);
+            return;
+        }
+        response.json() // 3
+        .then(data => {
+            console.info(data)
+            displayList(data)
+            querySuccess();
+        })//4
+    }).catch((err)=> {
+        console.error(`${err}`)
+        queryFailure();
+    }); // 5
+
+}
+
+function displayList(data){
+    console.log("Displaying List")
+    let parent = document.querySelector('.listDiv');
+    let div = document.createElement('div');
+    div.innerHTML = 'LIST ' + data.listId + ' ' + data.title + ', Done?: ' + data.completed;
+    parent.appendChild(div);
+}
+
+function displayItem(data){
+    console.log("Displaying Item")
+    let parent = document.querySelector('.listDiv');
+    let div = document.createElement('div');
+    div.innerHTML = data.itemId + ': ' + data.textBody + ', Done?: ' + data.completed;
+    parent.appendChild(div);
+}
+
+const querySuccess = () => {
+    let parent = document.querySelector('h1');
+    parent.innerHTML = "QUERY SUCCESS";
+}
+
+const queryFailure = () => {
+    let parent = document.querySelector('h1');
+    parent.innerHTML = "QUERY FALIURE";
+}
+
+const clearInfo = () => {
+    console.log("Information cleared")
+    let parent = document.querySelector('.listDiv');
+    parent.innerHTML = "";
 }
 
 function fetchPost1() {

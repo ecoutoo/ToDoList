b79c3d815034dae8b381c0255d94943cf95301f5
ecoutoo <viccontecouto@gmail.com>
2021-03-22 00:01:05 +0000

    TDL-29 #Create finished

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-let toDoListId = document.querySelector("#listNumForm");
+let id = document.querySelector("#listNumForm");
+
+let dataType = document.querySelector("#datatype");
+
+let textData = document.querySelector("#listNameForm");
+
+let doneBool = document.querySelector("#myCheck");
 
 function readAll() {
     clearInfo();
@@ -18,44 +24,19 @@ function readAll() {
             for (var i = 0; i < data.length; i++) {
                 displayList(data[i])
                 let currentId = data[i].listId
-                readAllItems(currentId);
             }
             querySuccess();
+            return;
         }) // 4
     }).catch((err)=> {
         console.error(`${err}`)
-        queryFailure();
     }); // 5
-}
-
-function readAllItems(listId){
-    fetch("http://localhost:8080/item", {
-        method: 'get',    
-        }) // 1  
-        .then((response) => {
-            if (response.status !== 200) {  //  2
-                console.error(`status: ${reponse.status}`);
-                return;
-            }
-            response.json() // 3
-            .then(data => {
-                console.info(data)
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i].identification.charAt(0) == listId) {
-                        displayItem(data[i])
-                      }
-                }
-                querySuccess();
-            }) // 4
-        }).catch((err)=> {
-            console.error(`${err}`)
-            queryFailure();
-        }); // 5
+    queryFailure();
 }
 
 function readOne() {
     clearInfo();
-    fetch("http://localhost:8080/todolist/" + toDoListId.value, {
+    fetch("http://localhost:8080/todolist/" + id.value, {
     method: 'get',    
     }) // 1  
     .then((response) => {
@@ -71,9 +52,124 @@ function readOne() {
         })//4
     }).catch((err)=> {
         console.error(`${err}`)
-        queryFailure();
     }); // 5
+    queryFailure();
+}
 
+function deleteOne(){
+    if (dataType.value == "listdata") {
+        fetch("http://localhost:8080/todolist/" + id.value, {
+            method: 'delete'
+        }).then(res => {
+            if (res.status === 200) {
+                console.info(id.value + " deleted")
+                querySuccess();
+                return;
+            } else {
+                console.error(`Request failed ${res.body}`)
+            }
+        }).catch((error) => console.error(`Request failed ${error}`))
+    }
+    else if (dataType.value == "itemdata"){
+        fetch("http://localhost:8080/item/" + id.value, {
+            method: 'delete'
+        }).then(res => {
+            if (res.status === 200) {
+                console.info(id.value + " deleted")
+                querySuccess();
+                return;
+            } else {
+                console.error(`Request failed ${res.body}`)
+            }
+        }).catch((error) => console.error(`Request failed ${error}`))
+    }
+    queryFailure();
+}
+
+function createOne(){
+    clearInfo();
+    if (dataType.value == "listdata") {
+        fetch("http://localhost:8080/todolist", {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": textData.value,
+                "completed": document.getElementById("myCheck").checked
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                displayList(data);
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    else if (dataType.value == "itemdata"){
+        fetch("http://localhost:8080/item", {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "textBody": textData.value,
+                "taskDone": document.getElementById("myCheck").checked,
+                "toDoList": {
+                    "listId": id.value
+                }
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    queryFailure();
+}
+
+function updateOne(){
+    clearInfo();
+    if (dataType.value == "listdata") {
+        fetch("http://localhost:8080/todolist", {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": textData.value,
+                "completed": document.getElementById("myCheck").checked
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                displayList(data);
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    else if (dataType.value == "itemdata"){
+        fetch("http://localhost:8080/item", {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "textBody": textData.value,
+                "taskDone": document.getElementById("myCheck").checked,
+                "toDoList": {
+                    "listId": id.value
+                }
+            })
+        }).then(res => res.json())
+            .then((data) => {
+                querySuccess();
+                return;
+            })
+            .catch((error) => console.error(`Request failed ${error}`))
+    }
+    queryFailure();
 }
 
 function displayList(data){
